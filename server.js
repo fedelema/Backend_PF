2a0cef742dc52375b668fc4992f385b6611c171b
Federico Lema <fedelema96@gmail.com>
2022-11-10 19:12:16 -0300

    Primer commit v5

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/hola', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hola Vercel!');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
     console.log(`Servidor express escuchando en el puerto ${PORT}`)
 });
